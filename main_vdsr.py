765808b32e35da42e3d7c8f75821b11d1d98cc13
Lin,Bo-Jun <b4435242.cs06@nctu.edu.tw>
2020-12-12 01:53:14 +0800

    tensor var

--- main_vdsr.py
+++ main_vdsr.py
@@ -138,8 +138,8 @@ def psnr(label, outputs, max_val=1.):
     https://en.wikipedia.org/wiki/Peak_signal-to-noise_ratio#Definition
     First we need to convert torch tensors to NumPy operable.
     """
-    label = label.cuda().detach().numpy()
-    outputs = outputs.cuda().detach().numpy()
+    label = label.cpu().detach().numpy()
+    outputs = outputs.cpu().detach().numpy()
     img_diff = outputs - label
     rmse = math.sqrt(np.mean((img_diff) ** 2))
     if rmse == 0:
